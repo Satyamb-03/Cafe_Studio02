da4efaedde89036425ff7e817fad5819fb6a946b
TannuTannuMalik <mallikayushi7178@gmail.com>
2024-06-03 17:46:44 +1200

    Promotion Updated

--- Promotional.Designer.cs
+++ Promotional.Designer.cs
@@ -28,12 +28,255 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.home_button = new System.Windows.Forms.Button();
+            this.menu_button = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // home_button
+            // 
+            this.home_button.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.home_button.Location = new System.Drawing.Point(562, 0);
+            this.home_button.Name = "home_button";
+            this.home_button.Size = new System.Drawing.Size(167, 87);
+            this.home_button.TabIndex = 0;
+            this.home_button.Text = "Home";
+            this.home_button.UseVisualStyleBackColor = true;
+            // 
+            // menu_button
+            // 
+            this.menu_button.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menu_button.Location = new System.Drawing.Point(840, 0);
+            this.menu_button.Name = "menu_button";
+            this.menu_button.Size = new System.Drawing.Size(143, 87);
+            this.menu_button.TabIndex = 1;
+            this.menu_button.Text = "Menu";
+            this.menu_button.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(1087, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 87);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Special";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(1324, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 87);
+            this.button4.TabIndex = 1;
+            this.button4.Text = "Cart";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Impact", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Teal;
+            this.label2.Location = new System.Drawing.Point(72, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(383, 120);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "GREAT COFFEE FOR \r\n    SOME JOY";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Bookman Old Style", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(555, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(788, 156);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "\"Welcome winter and celebrate Diwali\r\n at our cafe! Our seasonal menu features a " +
+    "\r\ndelightful blend of Diwali treats and cozy winter\r\n favourites, perfect for wa" +
+    "rming up festive spirit\". ";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Book Antiqua", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(140, 728);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(195, 82);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Add to Order";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Gold;
+            this.button2.Font = new System.Drawing.Font("Book Antiqua", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(687, 728);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(195, 82);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Add to Order";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button5.Font = new System.Drawing.Font("Book Antiqua", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(1146, 728);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(195, 82);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Add to Order";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Cafe_Studio02.Properties.Resources.seasonal_delicious_3;
+            this.pictureBox3.Location = new System.Drawing.Point(1011, 362);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(456, 271);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Cafe_Studio02.Properties.Resources.Seasonal_delicious;
+            this.pictureBox2.Location = new System.Drawing.Point(107, 353);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(386, 280);
+            this.pictureBox2.TabIndex = 7;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Cafe_Studio02.Properties.Resources.Seasonal_delicious2_;
+            this.pictureBox1.Location = new System.Drawing.Point(618, 378);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(323, 255);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Bookman Old Style", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(115, 650);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(329, 64);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Diwali Special Sweets \r\n     2 For $5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Bookman Old Style", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(672, 650);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(269, 64);
+            this.label4.TabIndex = 9;
+            this.label4.Text = " Vegan Salad Bowl\r\n    $7.99";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Bookman Old Style", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(1117, 650);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(251, 64);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "BUM BUM Donut \r\n     $2.50 only";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Bookman Old Style", 16.125F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Firebrick;
+            this.label6.Location = new System.Drawing.Point(609, 310);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(374, 50);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Today\'s Special";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Cafe_Studio02.Properties.Resources.logo;
+            this.pictureBox4.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(167, 106);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Promotional
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Promotional";
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(1518, 848);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.menu_button);
+            this.Controls.Add(this.home_button);
+            this.Name = "Promotional";
+            this.Text = "Promotion";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button home_button;
+        private System.Windows.Forms.Button menu_button;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox4;
     }
 }
