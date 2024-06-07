032de4c2ef5c043393d25c67da675d0f2d46eaa9
Ramandeep1003 <rdeep1032001@gmail.com>
2024-06-07 15:38:11 +1200

    Final changes in menu and cart.

--- Form2.cs
+++ Form2.cs
@@ -12,9 +12,18 @@ namespace Cafe_Studio02
 {
     public partial class Menu : Form
     {
+
+        private Cart _cart;
         public Menu()
         {
             InitializeComponent();
+            _cart = new Cart();
+        }
+
+        private void AddToCart(string itemName, decimal price)
+        {
+            _cart.AddItem(itemName, price);
+            MessageBox.Show($"{itemName} added to cart.");
         }
 
         private void Form2_Load(object sender, EventArgs e)
@@ -33,7 +42,7 @@ namespace Cafe_Studio02
 
         private void label8_Click(object sender, EventArgs e)
         {
-
+            AddToCart("Kis Fluffy", 5.00m);
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
@@ -43,7 +52,7 @@ namespace Cafe_Studio02
 
         private void chaiLatte_Click(object sender, EventArgs e)
         {
-
+            AddToCart("Chai Latte", 7.00m);
         }
 
         private void login_Click(object sender, EventArgs e)
@@ -73,67 +82,74 @@ namespace Cafe_Studio02
 
         private void Cappuccino_Click(object sender, EventArgs e)
         {
+            AddToCart("Cappuccino", 8.00m);
 
         }
 
         private void latte_Click(object sender, EventArgs e)
         {
-
+            AddToCart("Latte", 10.00m);
         }
 
         private void hChocolate_Click(object sender, EventArgs e)
         {
-
+            AddToCart("Hot Chocolate", 12.00m);
         }
 
         private void label1_Click(object sender, EventArgs e) // ice coffee
         {
-
+            AddToCart("Iced Coffee", 8.00m);
         }
 
         private void longBlack_Click(object sender, EventArgs e)
         {
-
+            AddToCart("Long Black", 15.00m);
         }
 
         private void flatWhite_Click(object sender, EventArgs e)
         {
-
+            AddToCart("Flat White", 10.00m);
         }
 
         private void dilmahTea_Click(object sender, EventArgs e)
         {
-
+            AddToCart("Dilmah Tea", 20.00m);
         }
 
         private void espresso_Click(object sender, EventArgs e)
         {
-
+            AddToCart("Espresso", 10.00m);
         }
 
         private void label5_Click(object sender, EventArgs e) // eggs benedict
         {
-
+            AddToCart("Eggs Benedict", 14.00m);
         }
 
         private void label4_Click(object sender, EventArgs e) // chicken strips
         {
-
+            AddToCart("Chicken Strips", 12.00m);
         }
 
         private void label3_Click(object sender, EventArgs e) // cheese sticks
         {
-
+            AddToCart("Cheese Sticks", 10.00m);
         }
 
         private void label2_Click(object sender, EventArgs e) // chilli fries
         {
-
+            AddToCart("Chilli Fries", 5.00m);
         }
 
         private void label6_Click(object sender, EventArgs e) // Donuts
         {
+            AddToCart("Donuts", 13.00m);
+        }
 
+        private void buttonCart_Click(object sender, EventArgs e)
+        {
+            CartForm cartForm = new CartForm(_cart);
+            cartForm.Show();
         }
     }
 }
